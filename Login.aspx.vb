300ff52e70eec9d956ecab4d6f427ab0e26d9862
unknown <mdvertola@gmail.com>
2020-12-05 18:37:56 -0700

    adding yung Mikey's files

---
+++ Login.aspx.vb
@@ -0,0 +1,16 @@
+﻿Public Class Login
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Private Sub Log_Authenticate(ByVal sender As Object, ByVal e As System.Web.UI.WebControls.AuthenticateEventArgs) Handles LoginCtrl.Authenticate
+        If LoginCtrl.UserName = "mikey" AndAlso LoginCtrl.Password = "mikey" Then
+            System.Web.HttpContext.Current.Session("username") = 'mikey'
+            e.Authenticated = True
+            Response.Redirect("Home.aspx")
+        End If
+    End Sub
+
+End Class
